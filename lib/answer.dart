8f2872fa056971f172358b64dae16f5427edb1e5
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-13 00:29:37 -0200

    Create answer widget

---
+++ lib/answer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String text;
+  final Function callback;
+  Answer(this.text, this.callback);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: RaisedButton(
+        child: Text(text),
+        onPressed: callback,
+      ),
+    );
+  }
+}
