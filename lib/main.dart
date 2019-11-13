d7ed7f9a466b1856e9edf2624322743517ce60f1
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-13 00:30:15 -0200

    Main component state logic

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  var _questions = [
+    {
+      'text': 'What\'s your favorite color?',
+      'answers': ['Black', 'White', 'Red', 'Green']
+    },
+    {
+      'text': 'What\'s your favorite animal?',
+      'answers': ['Cat', 'Dog', 'Bird', 'Fish']
+    }
+  ];
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.cyan,
+        buttonColor: Colors.cyan,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(children: [
+          Question(_questions[_questionIndex]['text']),
+          ...(_questions[_questionIndex]['answers'] as List<String>)
+              .map((answer) {
+            return Answer(answer, _answerQuestion);
+          }).toList()
+        ]),
+      ),
+    );
+  }
+}
